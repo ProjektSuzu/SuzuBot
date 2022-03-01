bfa8f8318ce8a40309a14e61f9f1ae338a638a6a
AkulaKirov <photonspk@gmail.com>
2022-03-02 03:36:32 +0800

    Implement BaseCommand

--- ProjektRin_Konata/Commands/BaseCommand.cs
+++ ProjektRin_Konata/Commands/BaseCommand.cs
@@ -2,6 +2,10 @@
 {
     public abstract class BaseCommand
     {
+        private bool _isEnabled;
+        public bool IsEnabled => _isEnabled;
         public abstract void OnInit();
+        public virtual void OnEnable() => _isEnabled = true;
+        public virtual void OnDisable() => _isEnabled = false;
     }
 }

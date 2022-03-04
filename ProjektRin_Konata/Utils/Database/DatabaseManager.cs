317fc3f7f038d0b3aac082737283a15355e27f43
AkulaKirov <photonspk@gmail.com>
2022-03-05 05:49:23 +0800

    Add DatabaseManager

---
+++ ProjektRin_Konata/Utils/Database/DatabaseManager.cs
@@ -0,0 +1,37 @@
+﻿using NLog;
+using ProjektRin.System;
+using ProjektRin.Utils.Database.Tables;
+using SQLite;
+
+namespace ProjektRin.Utils.Database
+{
+    internal class DatabaseManager
+    {
+        private static DatabaseManager _instance = new();
+        private DatabaseManager() 
+        {
+        }
+        public static DatabaseManager Instance => _instance;
+
+        private static string dbPath = Path.Combine(BotManager.rootPath, "database/rin.db");
+        public SQLiteConnection _db;
+
+        private static readonly string TAG = "DBMGR";
+        private static readonly Logger Logger = LogManager.GetLogger(TAG);
+
+        public bool OpenConnection()
+        {
+            Logger.Info($"Connecting to database: {dbPath}.");
+            if (!File.Exists(dbPath))
+            {
+                Logger.Error("Database not found.");
+                return false;
+            }
+            _db = new SQLiteConnection(dbPath);
+            _db.CreateTable<UserInfo>();
+            Logger.Info("Database connected.");
+            return true;
+        }
+
+    }
+}
---
+++ ProjektRin_Konata/Utils/Database/Tables/UserInfo.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektRin.Utils.Database.Tables
+{
+    [Table("T_USER_INFO")]
+    public class UserInfo
+    {
+        [PrimaryKey]
+        [Column("uin")]
+        public uint uin { get; set; }
+
+        [Column("coin")]
+        public uint coin { get; set; }
+
+        [Column("level")]
+        public int level { get; set; }
+
+        [Column("exp")]
+        public int exp { get; set; }
+
+        [Column("last_sign")]
+        public DateTime lastSign { get; set; }
+    }
+}

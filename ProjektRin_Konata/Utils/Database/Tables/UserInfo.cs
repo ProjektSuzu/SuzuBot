8345d595209b7dd000390ca9e6de1e387263d242
AkulaKirov <photonspk@gmail.com>
2022-03-06 19:22:35 +0800

    Move some function into UserInfoManager

--- ProjektRin_Konata/Utils/Database/Tables/UserInfo.cs
+++ ProjektRin_Konata/Utils/Database/Tables/UserInfo.cs
@@ -11,6 +11,31 @@ namespace ProjektRin.Utils.Database.Tables
     {
         private static SQLiteConnection _db = DatabaseManager.Instance.dbConnection;
 
+        public static int LevelToExp(int level)
+        {
+            return level * (level + 5) * 10;
+        }
+
+        public static string CoinToString(uint coin)
+        {
+            if (coin < 1000)
+            {
+                return $"{coin} Byte";
+            }
+            else if (coin < 1000000)
+            {
+                return $"{(float)coin / 1000:f3} KB";
+            }
+            else if (coin < 1000000000)
+            {
+                return $"{(float)coin / 1000000:f3} MB";
+            }
+            else
+            {
+                return $"{(float)coin / 1000000000:f3} GB";
+            }
+        }
+
         public static bool UpdateUserInfo(UserInfo info)
         {
             var result = _db

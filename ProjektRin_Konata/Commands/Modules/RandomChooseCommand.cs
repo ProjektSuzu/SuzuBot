64a4839838ef10cd3c88d5ac39571da8dd67dc15
AkulaKirov <photonspk@gmail.com>
2022-03-07 01:36:59 +0800

    Add RandomChooseCommand

---
+++ ProjektRin_Konata/Commands/Modules/RandomChooseCommand.cs
@@ -0,0 +1,43 @@
+﻿using Konata.Core;
+using Konata.Core.Events.Model;
+using Konata.Core.Message;
+using ProjektRin.Attributes.Command;
+using ProjektRin.Attributes.CommandSet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektRin.Commands.Modules
+{
+    [CommandSet("随机数")]
+    public class RandomChooseCommand : BaseCommand
+    {
+        public override string Help =>
+            "";
+
+        public override void OnInit()
+        {
+
+        }
+
+        [GroupMessageCommand("随机选择", new[] { @"^random\s?([\s\S]+)?", @"^帮我选\s?([\s\S]+)?" })]
+        public void OnRandomChoose(Bot bot, GroupMessageEvent messageEvent, List<string> args)
+        {
+            var reply = "";
+            var random = new Random().Next(99);
+            if (random == 49)
+            {
+                reply = "\n铃建议什么都不做呢";
+                bot.SendGroupMessage(messageEvent.GroupUin, new MessageBuilder().At(messageEvent.MemberUin).Text(reply));
+                return;
+            }
+            reply = "\n铃的建议是: ";
+            reply += args[new Random().Next(args.Count())];
+            bot.SendGroupMessage(messageEvent.GroupUin, new MessageBuilder().At(messageEvent.MemberUin).Text(reply));
+            return;
+        }
+
+    }
+}

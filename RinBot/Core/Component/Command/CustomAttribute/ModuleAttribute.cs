7e6687c6265850e131224e631cb06f90167aa8e5
AkulaKirov <photonspk@gmail.com>
2022-07-05 22:28:04 +0800

    更新模块注解

--- RinBot/Core/Component/Command/CustomAttribute/ModuleAttribute.cs
+++ RinBot/Core/Component/Command/CustomAttribute/ModuleAttribute.cs
@@ -13,12 +13,14 @@
         public string ModuleName { get; private set; }
         public string ModuleID { get; private set; }
         public ModuleEnableConfig ModuleEnableConfig { get; private set; }
+        public bool CriticalModule { get; private set; }
 
-        public ModuleAttribute(string moduleName, string moduleID, ModuleEnableConfig moduleEnableConfig = ModuleEnableConfig.NormallyEnable)
+        public ModuleAttribute(string moduleName, string moduleID, ModuleEnableConfig moduleEnableConfig = ModuleEnableConfig.NormallyEnable, bool critical = false)
         {
             ModuleName = moduleName;
             ModuleID = moduleID;
             ModuleEnableConfig = moduleEnableConfig;
+            CriticalModule = critical;
         }
     }
 }

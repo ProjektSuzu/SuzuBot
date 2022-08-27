3636a4f20c3900dca3b422da30bad8695c304e7b
AkulaKirov <photonspk@gmail.com>
2022-08-27 21:24:56 +0800

    功能迁移

--- RinBot/Command/Core.cs
+++ RinBot/Command/Core.cs
@@ -209,25 +209,5 @@ namespace RinBot.Command
                 messageEvent.Reply(builder.ToString());
             }
         }
-
-        [TextCommand("模块重载", "reload", UserPermission.Root)]
-        public void OnReload(MessageEventArgs messageEvent)
-        {
-            GlobalScope.CommandManager.ReloadModules();
-            messageEvent.Reply($"[CMD]\n载入了 {CommandManager.Instance.ModuleCount} 个模块, {CommandManager.Instance.CommandCount} 个命令.");
-        }
-
-        [TextCommand("查看日志", "log", UserPermission.Root)]
-        public void OnPeekLog(MessageEventArgs messageEvent)
-        {
-            string path = Path.Combine(GlobalScope.ROOT_DIR_PATH, "log", "log.txt");
-            FileStream fs = new(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            StreamReader streamReader = new(fs, Encoding.UTF8);
-            var log = streamReader.ReadToEnd()
-                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
-                .TakeLast(20);
-            messageEvent.Reply(string.Join(Environment.NewLine, log));
-        }
-
     }
 }
--- RinBot/Command/DeveloperOption.cs
+++ RinBot/Command/DeveloperOption.cs
@@ -1,10 +1,32 @@
-﻿using RinBot.Core.Components.Attributes;
+﻿using RinBot.Core.Components;
+using RinBot.Core;
+using RinBot.Core.Components.Attributes;
+using RinBot.Core.Components.Managers;
+using RinBot.Core.KonataCore.Events;
+using System.Text;
 
 namespace RinBot.Command
 {
     [Module("开发者选项", "AkulaKirov.DeveloperOption", true)]
     internal class DeveloperOption
     {
+        [TextCommand("模块重载", "reload", UserPermission.Root)]
+        public void OnReload(MessageEventArgs messageEvent)
+        {
+            GlobalScope.CommandManager.ReloadModules();
+            messageEvent.Reply($"[CMD]\n载入了 {CommandManager.Instance.ModuleCount} 个模块, {CommandManager.Instance.CommandCount} 个命令.");
+        }
 
+        [TextCommand("查看日志", "log", UserPermission.Root)]
+        public void OnPeekLog(MessageEventArgs messageEvent)
+        {
+            string path = Path.Combine(GlobalScope.ROOT_DIR_PATH, "log", "log.txt");
+            FileStream fs = new(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            StreamReader streamReader = new(fs, Encoding.UTF8);
+            var log = streamReader.ReadToEnd()
+                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .TakeLast(20);
+            messageEvent.Reply(string.Join(Environment.NewLine, log));
+        }
     }
 }

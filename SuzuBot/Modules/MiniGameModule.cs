856a077141a42e4ea6b8cfef21b7a6be9488f74f
AkulaKirov <photonspk@gmail.com>
2023-01-19 02:43:10 +0800

    新增小游戏模块

---
+++ SuzuBot/Modules/MiniGameModule.cs
@@ -0,0 +1,28 @@
+﻿using Konata.Core.Message;
+using Konata.Core.Message.Model;
+using SuzuBot.Core.Attributes;
+using SuzuBot.Core.EventArgs.Message;
+using SuzuBot.Core.Modules;
+
+namespace SuzuBot.Modules;
+public class MiniGameModule : BaseModule
+{
+	public MiniGameModule()
+	{
+		Name = "娱乐小游戏";
+	}
+
+	[Command("抽老婆", "^roll_waifu$", "^抽老婆$")]
+	public async Task RollMemberAsWaifu(GroupMessageEventArgs eventArgs, string[] args)
+	{
+        var seed = int.Parse(DateTime.Today.ToString("yyyyMMdd")) + eventArgs.Sender.Id;
+        var rnd = new Random((int)seed);
+
+		var members = eventArgs.Group.Value.Members.OrderBy(x => x.Id).ToList();
+		var member = members[rnd.Next(members.Count - 1)];
+		MessageBuilder builder = new("今天你的群友老婆是\n");
+		builder.Add(ImageChain.CreateFromUrl(member.AvatarUrl));
+		builder.Text(member.Name);
+		await eventArgs.Reply(builder);
+    }
+}

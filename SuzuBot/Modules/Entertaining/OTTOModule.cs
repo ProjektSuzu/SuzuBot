9aa531d342a1b07387ed5750b8a36e1c82cfaf60
AkulaKirov <photonspk@gmail.com>
2023-01-21 01:28:14 +0800

    完善活字印刷功能

--- SuzuBot/Modules/Entertaining/OTTOModule.cs
+++ SuzuBot/Modules/Entertaining/OTTOModule.cs
@@ -1,7 +1,9 @@
 ﻿using Konata.Core.Message;
+using Konata.Core.Message.Model;
 using SuzuBot.Core.Attributes;
 using SuzuBot.Core.EventArgs.Message;
 using SuzuBot.Core.Modules;
+using SuzuBot.Utils;
 
 namespace SuzuBot.Modules.Entertaining;
 public class OTTOModule : BaseModule
@@ -17,12 +19,53 @@ public class OTTOModule : BaseModule
         Name = "电棍活字印刷";
     }
 
-    [Command("说的道理", "^sddl$")]
+    [Command("说的道理", "^otto (.*)")]
     public async Task RollMemberAsWaifu(GroupMessageEventArgs eventArgs, string[] args)
     {
         var builder = new MessageBuilder();
-        builder.Record(Path.Combine(ResourceDirPath, "sddl.mp3"));
+        var audio = await GetTTS(args[0]);
+        if (audio is null)
+        {
+            builder.Text("生成失败");
+            await eventArgs.SendMessage(builder);
+            return;
+        }
+        builder.Add(RecordChain.Create(audio));
         await eventArgs.SendMessage(builder);
     }
 
+    private async Task<byte[]?> GetTTS(string text)
+    {
+        var post = new MultipartFormDataContent
+        {
+            { new StringContent(text), "text" },
+            { new StringContent("true"), "inYsddMode" },
+            { new StringContent("true"), "norm" },
+            { new StringContent("false"), "reverse" },
+            { new StringContent("1"), "speedMult" },
+            { new StringContent("1"), "pitchMult" },
+        };
+        try
+        {
+            var result = await _httpClient.PostAsync("make", post);
+            if (!result.IsSuccessStatusCode)
+                return null;
+
+            var id = result.Content.ReadAsStringAsync().Result.DeserializeJson<TTSResponse>().id;
+            var bytes = await _httpClient.GetByteArrayAsync($"get/{id}.mp3");
+            return bytes;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+}
+
+
+public class TTSResponse
+{
+    public int code { get; set; }
+    public string id { get; set; }
 }

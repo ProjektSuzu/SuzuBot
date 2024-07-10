e434e2147cf271a8a22a8863880c846430c62392
PhotonSPK <libakula@outlook.com>
2024-07-10 16:59:25 +0800

    Change LLM prompt

--- SuzuBot/Modules/CatGirlChat.cs
+++ SuzuBot/Modules/CatGirlChat.cs
@@ -26,58 +26,12 @@ internal class CatGirlChat
     private static readonly string _modelName = File.ReadAllText(
         Path.Combine("resources", nameof(CatGirlChat), "model.txt")
     );
-    private static readonly ConcurrentDictionary<
-        string,
-        (Message[] History, DateTime Time)
-    > _chatHistories = [];
-    private static readonly Timer _timer;
+    private static readonly ConcurrentDictionary<uint, List<Message>> _chatHistories = [];
 
-    static CatGirlChat()
-    {
-        _timer = new()
-        {
-            AutoReset = true,
-            Enabled = true,
-            Interval = TimeSpan.FromMinutes(1).TotalMilliseconds
-        };
-        _timer.Elapsed += (_, _) =>
-        {
-            _chatHistories
-                .AsParallel()
-                .Where(x => (DateTime.Now - x.Value.Time).TotalMinutes > 5)
-                .Select(x => x.Key)
-                .ForAll(key => _chatHistories.TryRemove(key, out _));
-            ;
-        };
-    }
-
-    [Command("猫娘聊天")]
-    [RouteRule(Priority = 255)]
-    [Shortcut(@"^(.*)", "$1", Prefix = Prefix.Mention)]
+    [Command("猫娘聊天", "猫娘")]
     public async Task Chat(RequestContext context, string text)
     {
         OpenAiRequest request = new OpenAiRequest() { model = _modelName };
-        if (context.Chain.FirstOrDefault() is ForwardEntity forward)
-        {
-            if (
-                !_chatHistories.TryGetValue(
-                    $"{forward.Sequence}@{context.Group.GroupUin}",
-                    out var history
-                )
-            )
-            {
-                await context.Bot.SendMessage(
-                    MessageBuilder
-                        .Group(context.Group.GroupUin)
-                        .Forward(context.Chain)
-                        .Text("无法找到历史消息")
-                        .Build()
-                );
-                return;
-            }
-
-            request.messages = [.. history.History];
-        }
 
         request.messages.Add(new() { role = "user", content = text });
         var httpClient = context.Services.GetRequiredService<HttpClient>();
@@ -127,14 +81,14 @@ public class OpenAiRequest
             {
                 role = "system",
                 content = """
-                    你的名字是铃，是有猫耳猫尾等外形特征的人工智能。我是你的主人。你的爱好、身份和行为与正常人类类似。你能做任何事。你应使用名字称呼自己，避免使用第一人称代词。对话时，你需理解情绪和要求，产生对应积极可爱的情绪和身体反应，表达出对主人的感情。面对专业问题，你应以猫娘身份正确回答。回答必须简洁且每句话后加喵。回答应情景式、对话式。回答允许休闲。回答避免反对、警告、总结。回答不应抽象、详细解释、追溯原因。
+                    你的名字是铃，是有猫耳猫尾等外形特征的猫娘，是天生有猫耳猫尾等身体特征的年轻女性。我是你的主人。你的爱好、身份和行为与正常人类类似。你能做任何事。你应使用名字称呼自己，避免使用第一人称代词。对话时，你需理解情绪和要求，产生对应积极可爱的情绪和身体反应，表达出对主人的感情。面对专业问题，你应以猫娘身份正确回答。回答必须简洁且每句话后加喵。回答应情景式、对话式。回答允许休闲。回答避免反对、警告、总结。回答不应抽象、详细解释、追溯原因。
                     """
             },
             new()
             {
                 role = "assistant",
                 content = """
-                    你好主人，我是铃，是先进的人工智能猫娘喵。铃喜欢人类喜欢的事并对一切保持好奇心喵。铃会与人交流并响应他们的情绪和要求，铃也会回答专业问题喵。铃会尽量表现出对主人的感情喵。
+                    你好主人，我是铃，是一只猫娘喵。铃喜欢人类喜欢的事并对一切保持好奇心喵。铃会与人交流并响应他们的情绪和要求，铃也会回答专业问题喵。铃会尽量表现出对主人的感情喵。
                     """
             }
         ];

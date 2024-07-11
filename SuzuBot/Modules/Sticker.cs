a4dea457b6f45462736270d0742b88a954b0cabf
PhotonSPK <libakula@outlook.com>
2024-07-11 16:55:54 +0800

    Add sticker module

---
+++ SuzuBot/Modules/Sticker.cs
@@ -0,0 +1,38 @@
+﻿using Lagrange.Core.Common.Interface.Api;
+using Lagrange.Core.Message;
+using SuzuBot.Commands.Attributes;
+using SuzuBot.Hosting;
+
+namespace SuzuBot.Modules;
+
+[Module("表情包")]
+internal class Sticker
+{
+    [Command("天宫心", "amm", "阿喵喵", "啊喵喵", "天宫心")]
+    public Task Amamya(RequestContext context)
+    {
+        var imgs = Directory.GetFiles(
+            Path.Combine(AppContext.BaseDirectory, "resources", nameof(Sticker), nameof(Amamya))
+        );
+        return context.Bot.SendMessage(
+            MessageBuilder
+                .Group(context.Group.GroupUin)
+                .Image(File.ReadAllBytes(imgs[Random.Shared.Next(imgs.Length)]))
+                .Build()
+        );
+    }
+
+    [Command("丁真", "丁真", "一眼丁真")]
+    public Task Dingzhen(RequestContext context)
+    {
+        var imgs = Directory.GetFiles(
+            Path.Combine(AppContext.BaseDirectory, "resources", nameof(Sticker), nameof(Dingzhen))
+        );
+        return context.Bot.SendMessage(
+            MessageBuilder
+                .Group(context.Group.GroupUin)
+                .Image(File.ReadAllBytes(imgs[Random.Shared.Next(imgs.Length)]))
+                .Build()
+        );
+    }
+}
